51d032f31871620e21236687337bc0d8c295146a
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-22 13:33:05 +0500

    add test genre all

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ from unittest.mock import MagicMock
 import pytest
 
 from dao.director import DirectorDAO
+from dao.genre import GenreDAO
 from dao.model.director import Director
+from dao.model.genre import Genre
 
 
 @pytest.fixture()
@@ -21,3 +23,19 @@ def director_dao():
     director_dao.update = MagicMock()
 
     return director_dao
+
+@pytest.fixture()
+def genre_dao():
+    genre_dao = GenreDAO(None)
+
+    genre_1 = Genre(id=1, name="Comedy")
+    genre_2 = Genre(id=2, name="Roman")
+    genre_3 = Genre(id=3, name="Drama")
+
+    genre_dao.get_one = MagicMock(return_value=genre_1)
+    genre_dao.get_all = MagicMock(return_value=[genre_1, genre_2])
+    genre_dao.create = MagicMock(return_value=genre_3)
+    genre_dao.delete = MagicMock()
+    genre_dao.update = MagicMock()
+
+    return genre_dao
